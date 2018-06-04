82cec8fd8f8e8393ed8b6506646fc02d68d6fb9b
flewczak <filip.lewczak@gmail.com>
2018-06-05 00:58:07 +0200

    location monitoring

--- app/src/main/java/com/filip/focushelper2/MonitoringService.java
+++ app/src/main/java/com/filip/focushelper2/MonitoringService.java
@@ -10,6 +10,10 @@ import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Button;
@@ -34,6 +38,8 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.TreeMap;
 
+import static java.lang.StrictMath.abs;
+
 //IntentService
 public class MonitoringService extends IntentService {
     public MonitoringService() {
@@ -41,6 +47,128 @@ public class MonitoringService extends IntentService {
 //        super();
 
     }
+
+    //location service start
+
+    private static final String TAG = "TESTGPS";
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 10;
+
+    private Location myLocation;
+
+    private class LocationListener implements android.location.LocationListener
+    {
+        Location mLastLocation;
+
+        public LocationListener(String provider)
+        {
+            Log.wtf(TAG, "LocationListener " + provider);
+            mLastLocation = new Location(provider);
+            myLocation = mLastLocation;
+        }
+
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            //tutaj trzeba weryfikować czy zmina spowodowała dopisanie nowych rzeczy do listy
+            Log.wtf(TAG, "onLocationChanged: " + location);
+            mLastLocation.set(location);
+            myLocation.set(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider)
+        {
+            Log.wtf(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider)
+        {
+            Log.wtf(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras)
+        {
+            Log.wtf(TAG, "onStatusChanged: " + provider);
+        }
+    }
+
+    LocationListener[] mLocationListeners = new LocationListener[] {
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+
+//    @Override
+//    public int onStartCommand(Intent intent, int flags, int startId)
+//    {
+//        Log.e(TAG, "onStartCommand");
+//        super.onStartCommand(intent, flags, startId);
+//        return START_STICKY;
+//    }
+
+    @Override
+    public void onCreate()
+    {
+        Log.e(TAG, "onCreate");
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[1]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
+        }
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE,
+                    mLocationListeners[0]);
+        } catch (java.lang.SecurityException ex) {
+            Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Log.e(TAG, "onDestroy");
+        super.onDestroy();
+        if (mLocationManager != null) {
+            for (int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (Exception ex) {
+                    Log.i(TAG, "fail to remove location listners, ignore", ex);
+                }
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        Log.e(TAG, "initializeLocationManager");
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+
+    //location service end
+
+
+
+
 
 //     @Nullable
 //     @Override
@@ -201,27 +329,62 @@ public class MonitoringService extends IntentService {
 //        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
 //        Date d = new Date();
 //        String dayOfTheWeek = sdf.format(d);
-        if(isCurrentDayOftheWeek(sharedPreferences)==false) {
-            return false;
+
+        boolean type = sharedPreferences.getBoolean("type",false);
+        if(type==false) {
+
+
+            if (isCurrentDayOftheWeek(sharedPreferences) == false) {
+                return false;
+            }
+            int hoursStart = sharedPreferences.getInt("hoursStart", -1);
+            int minutesStart = sharedPreferences.getInt("minutesStart", -1);
+            int hoursStop = sharedPreferences.getInt("hoursStop", -1);
+            int minutesStop = sharedPreferences.getInt("minutesStop", -1);
+            //no settings
+            if (hoursStart == -1 || minutesStart == -1 || hoursStop == -1 || minutesStop == -1) {
+                return false;
+            }
+            Calendar mcurrentTime = Calendar.getInstance();
+            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+            int minute = mcurrentTime.get(Calendar.MINUTE);
+            //not in time window
+            if (!((hoursStart < hour || (hoursStart == hour && minutesStart <= minute))
+                    && (hoursStop > hour || (hoursStop == hour && minutesStop >= minute)))) {
+                return false;
+            }
+            return true;
         }
-        int hoursStart = sharedPreferences.getInt("hoursStart",-1);
-        int minutesStart = sharedPreferences.getInt("minutesStart",-1);
-        int hoursStop = sharedPreferences.getInt("hoursStop",-1);
-        int minutesStop = sharedPreferences.getInt("minutesStop",-1);
-        //no settings
-        if(hoursStart==-1 || minutesStart==-1 || hoursStop==-1 || minutesStop==-1) {
-            return false;
-        }
-        Calendar mcurrentTime = Calendar.getInstance();
-        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-        int minute = mcurrentTime.get(Calendar.MINUTE);
-        //not in time window
-        if(!((hoursStart<hour||(hoursStart==hour&&minutesStart<=minute))
-                &&(hoursStop>hour||(hoursStop==hour&&minutesStop>=minute)))) {
-            return false;
+        //location
+        else {
+            float latitude = sharedPreferences.getFloat("latitude",0);
+            float longitude = sharedPreferences.getFloat("longitude",0);
+            float myLocationLatitude= (float) myLocation.getLatitude();
+            float myLocationLongitude= (float) myLocation.getLongitude();
+            if(abs(latitude-myLocationLatitude)<0.00500&&abs(longitude-myLocationLongitude)<0.00500) {
+                //is in the range
+                return true;
+            }
+            else {
+                return false;
+            }
         }
 
-        return true;
+        //location
+//        boolean type = sharedPreferences.getBoolean("type",false);
+//        if(type==false) {
+//            //nothing - not map -standard mode
+//            return true;
+//        }
+//        else {
+//            float latitude = sharedPreferences.getFloat("latitude",0);
+//            float longitude = sharedPreferences.getFloat("longitude",0);
+//            float myLocationLatitude= (float) myLocation.getLatitude();
+//            float myLocationLongitude= (float) myLocation.getLongitude();
+//
+//        }
+
+//        return true;
     }
 
     private boolean isCurrentDayOftheWeek (SharedPreferences sharedPreferences) {
