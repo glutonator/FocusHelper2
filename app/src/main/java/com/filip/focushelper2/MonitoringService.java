db8b418bd25477ff2994314b51de396bb12ceae4
flewczak <filip.lewczak@gmail.com>
2018-06-05 20:04:48 +0200

    Accelerometer

--- app/src/main/java/com/filip/focushelper2/MonitoringService.java
+++ app/src/main/java/com/filip/focushelper2/MonitoringService.java
@@ -2,6 +2,9 @@ package com.filip.focushelper2;
 
 import android.app.ActivityManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
@@ -10,11 +13,16 @@ import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
@@ -39,14 +47,79 @@ import java.util.TimerTask;
 import java.util.TreeMap;
 
 import static java.lang.StrictMath.abs;
+import android.os.Handler;
 
 //IntentService
-public class MonitoringService extends IntentService {
+public class MonitoringService extends IntentService implements SensorEventListener {
     public MonitoringService() {
         super("MonitoringService");
 //        super();
 
     }
+
+    //accelometr
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private float mAccel; // acceleration apart from gravity
+    private float mAccelCurrent; // current acceleration including gravity
+    private float mAccelLast; // last acceleration including gravity
+
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        return null;
+//    }
+
+//    @Override
+//    public int onStartCommand(Intent intent, int flags, int startId) {
+//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        mAccelerometer = mSensorManager
+//                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        mSensorManager.registerListener(this, mAccelerometer,
+//                SensorManager.SENSOR_DELAY_UI, new Handler());
+//        return START_STICKY;
+//    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        mAccelLast = mAccelCurrent;
+        mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+        float delta = mAccelCurrent - mAccelLast;
+        mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+
+        if (mAccel > 11) {
+            showNotification();
+        }
+    }
+
+    /**
+     * show notification when Accel is more then the given int.
+     */
+    private void showNotification() {
+        final NotificationManager mgr = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder note = new NotificationCompat.Builder(this);
+        note.setContentTitle("Device Accelerometer Notification");
+        note.setTicker("New Message Alert!");
+        note.setAutoCancel(true);
+        // to set default sound/light/vibrate or all
+        note.setDefaults(Notification.DEFAULT_ALL);
+        // Icon to be set on Notification
+        note.setSmallIcon(R.mipmap.ic_launcher);
+        // This pending intent will open after notification click
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this,
+                MainActivity.class), 0);
+        // set pending intent to notification builder
+        note.setContentIntent(pi);
+        mgr.notify(101, note.build());
+    }
+
 
     //location service start
 
@@ -193,6 +266,14 @@ public class MonitoringService extends IntentService {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        //accelenemetr
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer,
+                SensorManager.SENSOR_DELAY_UI, new Handler());
+
+        //standard
         Toast.makeText(this, "onStartCommand!", Toast.LENGTH_SHORT).show();
         final List<String> stalkList = new ArrayList<>();
 //        stalkList.add("com.filip.focushelper2");
